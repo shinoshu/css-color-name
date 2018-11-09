f81d1e3e0783f794d1e17c7eb35f649041fedee2
Shuma Shinozaki <shu.shinozaki@gmail.com>
2018-11-09 13:17:16 +0900

    update

--- index.js
+++ index.js
@@ -1,7 +1,16 @@
-$(function () {
+const $ = require('jquery');
+const colors = require('color-name');
+
+const append = function(color) {
   $('<div>').css({
     'width': '30px',
     'height': '30px',
-    'background-color': 'aliceblue'
+    'background-color': color
   }).appendTo('body');
+};
+
+$(function () {
+  Object.keys(colors).forEach(function (color) {
+    append(color);
+  });
 });
